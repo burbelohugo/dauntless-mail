353a6c6557368998a30f4427df9202a7e94cd680
Hugo Burbelo <hburbelo@phone2action.com>
2021-02-20 18:50:55 -0800

    sws

--- gmailjs-node-boilerplate-master/src/extension.js
+++ gmailjs-node-boilerplate-master/src/extension.js
@@ -24,5 +24,30 @@ function startExtension(gmail) {
             const emailData = gmail.new.get.email_data(domEmail);
             console.log("Email data:", emailData);
         });
+
+        gmail.observe.before('send_message', function(url, body, data, xhr){
+            console.log('hello')
+            var body_params = xhr.xhrParams.body_params;
+          
+            // // lets cc this email to someone extra if the subject is 'Fake example'
+            // if(data.subject == 'Fake example') {
+            //   if(body_params.cc) {
+            //     if(typeof body_params.cc != 'object') body_params.cc = [ body_params.cc ];
+            //   } else {
+            //     body_params.cc = [];
+            //   }
+            //   body_params.cc.push('hburbelo@gmail.com');
+            // }
+            console.log(url)
+          console.log(data);
+          console.log(body);
+          console.log(xhr);
+        //   body.content_html = '"<div dir="ltr">always.</div>"'
+            const p = document.getElementsByClassName('Am Al');
+            console.log(p)
+            // now change the subject
+            // data.subject = 'Subject overwritten!';
+            console.log("sending message, url:", url, 'body', body, 'email_data', data, 'xhr', xhr);
+          });
     });
 }
