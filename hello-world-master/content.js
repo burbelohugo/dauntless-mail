de4dd478c12398b02956174772e7329c75cada89
Hugo Burbelo <hburbelo@phone2action.com>
2021-02-20 19:35:48 -0800

    Add editable html

--- hello-world-master/content.js
+++ hello-world-master/content.js
@@ -1,16 +1,21 @@
-InboxSDK.load('1', 'Hello World!').then(function(sdk){
+InboxSDK.load('1', 'sdk_hugoburbelo_fff6af6058').then(function(sdk){
 
 	// the SDK has been loaded, now do something with it!
 	sdk.Compose.registerComposeViewHandler(function(composeView){
 
 		// a compose view has come into existence, do something with it!
-		composeView.addButton({
-			title: "My Nifty Button!",
-			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
-			onClick: function(event) {
-				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-			},
-		});
+		// composeView.addButton({
+		// 	title: "My Nifty Button!",
+		// 	iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
+		// 	onClick: function(event) {
+		// 		event.composeView.insertTextIntoBodyAtCursor('Hello World!');
+		// 	},
+		// });
+
+		composeView.on('presending', function(event) {
+			console.log('Recipients have changed to: ' + event);
+			composeView.setBodyHTML("<div>Hugo is cool</div>")
+		  });
 
 	});
 
