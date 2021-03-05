3a5fca2277c9d0b8b35e9ab3659b3e053184af5b
Hugo Burbelo <hburbelo@phone2action.com>
2021-03-04 19:50:00 -0800

    Add new files

--- backend-v3/index.js
+++ backend-v3/index.js
@@ -1,11 +1,47 @@
 const express = require('express')
+const fs = require('fs');
+const text2png = require('text2png');
+const AWS = require('aws-sdk');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
 
 const port = process.env.PORT || 5000;
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 app.get('/', (req, res) => {
-    res.send('Hello from App Engine!');
+    const s3 = new AWS.S3();
+    const id = uuidv4();
+    const fileName = id + '.png';
+    const emailText = decodeURIComponent(req.query.content);
+    latestText = emailText;
+    latest = id;
+
+    fs.writeFileSync(fileName, text2png(emailText, {color: 'black', font: '13px Sans Serif'}));
+    const fileContent = fs.readFileSync(fileName);
+
+        // Setting up S3 upload parameters
+        const params = {
+            Bucket: 'technica-brand-assets',
+            Key: fileName, // File name you want to save as in S3
+            Body: fileContent,
+            ACL: 'public-read'
+        };
+
+        // Uploading files to the bucket
+        s3.upload(params, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+            res.send(data.Location);
+        });
 });
 
 app.listen(port, () => {
