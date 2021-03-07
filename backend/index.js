5d55a9b358ca1883645d84c31c8a0d5ffaf87c04
Hugo Burbelo <hburbelo@phone2action.com>
2021-03-07 13:22:08 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 const EMAIL_IMAGE_S3_BUCKET_NAME = 'blundr-prod';
 
 const app = express()
+let emailText = '';
+let latest = '';
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
