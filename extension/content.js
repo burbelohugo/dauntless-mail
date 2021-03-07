6fe04d1aec71f1b024da94ccdf3d4d76dbbd5458
Hugo Burbelo <hburbelo@phone2action.com>
2021-03-07 15:37:55 -0800

    Refactor extension JS

--- extension/content.js
+++ extension/content.js
@@ -1,35 +1,25 @@
-InboxSDK.load('1', 'sdk_hugoburbelo_fff6af6058').then(function(sdk){
+const INBOX_SDK_KEY = 'sdk_hugoburbelo_fff6af6058';
+const BACKEND_SERVICE_URL = 'https://prod3.dauntless.click';
 
-	// the SDK has been loaded, now do something with it!
+
+InboxSDK.load('1', INBOX_SDK_KEY).then(function(sdk){
 	sdk.Compose.registerComposeViewHandler(function(composeView){
-
-		// a compose view has come into existence, do something with it!
-		// composeView.addButton({
-		// 	title: "My Nifty Button!",
-		// 	iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
-		// 	onClick: function(event) {
-		// 		event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-		// 	},
-		// });
-
-		function httpGet(theUrl)
-		{
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-			xmlHttp.send( null );
+		function httpGet(url) {
+			const xmlHttp = new XMLHttpRequest();
+			xmlHttp.open("GET", url, false);
+			xmlHttp.send(null);
 			return xmlHttp.responseText;
 		}
 
-
 		composeView.on('presending', function(event) {
-			console.log('Recipients have changed to: ' + event);
-			// 
 			console.log(composeView.getHTMLContent())
-			const imgUrl = httpGet('https://prod3.dauntless.click?content=' + encodeURIComponent(composeView.getTextContent()));
+			const requestContent = encodeURIComponent(composeView.getHTMLContent());
+			const imgUrl = httpGet(`${BACKEND_SERVICE_URL}?content=${requestContent}`);
+
+			console.log('Successfully replaced text content with image.')
 			console.log(imgUrl)
+
 			composeView.setBodyHTML(`<div><img src="${imgUrl}"/></div>`)
 		  });
-
 	});
-
 });
