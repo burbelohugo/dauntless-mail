e41d0d7768f5fae97c32755f033461fe70e57743
Hugo Burbelo <hburbelo@phone2action.com>
2021-03-10 14:57:18 -0800

    Add white text below for searchability

--- extension/content.js
+++ extension/content.js
@@ -14,12 +14,22 @@ InboxSDK.load('1', INBOX_SDK_KEY).then(function(sdk){
 		composeView.on('presending', function(event) {
 			console.log(composeView.getHTMLContent())
 			const requestContent = encodeURIComponent(composeView.getHTMLContent());
+			const requestText = composeView.getTextContent();
 			const imgUrl = httpGet(`${BACKEND_SERVICE_URL}?content=${requestContent}`);
 
 			console.log('Successfully replaced text content with image.')
 			console.log(imgUrl)
 
-			composeView.setBodyHTML(`<div><img src="${imgUrl}"/></div>`)
+			composeView.setBodyHTML(`
+				<div>
+					<img src="${imgUrl}"/>
+				</div>
+				<div>
+					<div style='color: white; font-size: 1px; display: none;'>
+					${requestText}
+					</div>
+				</div>
+			`)
 		  });
 	});
 });
